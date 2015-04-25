cde117c69facd3aae1e9a173f892c5a4e631417f
aarkhipov <aarkhipov1991@gmail.com>
2015-04-25 15:12:44 +0400

    add examples of usages

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,42 @@
-## Pair of functions which allow you reduce calculations
+## Pair of functions which allow you reduce computations
 ## by caching the result of a matrix inversion.
+## 
+## Usage example:
+## 
+## > x = matrix(1:4, 2, 2)
+## > x_cache = makeCacheMatrix(x)
+## 
+## ## At first call the "'Calculate inversion of the matrix'" message will be displayed
+## > cacheSolve(x_cache) 
+## Calculate inversion of the matrix
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## 
+## ## At any other call the "'Calculate inversion of the matrix'" will not be displayed
+## ## because the cached inversion will be returned without new calculation
+## 
+## > cacheSolve(x_cache) 
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+##
+## ## You can set an new matrix to the cache object. 
+## ## In this case inversion will be calculated once again.
+##
+## > x_cache$set( matrix(17:20, 2, 2) )
+##
+## > cacheSolve(x_cache)
+## Calculate inversion of the matrix
+##      [,1] [,2]
+## [1,]  -10  9.5
+## [2,]    9 -8.5
+##
+## > cacheSolve(x_cache)
+##      [,1] [,2]
+## [1,]  -10  9.5
+## [2,]    9 -8.5
+
 
 
 ## Create a "special" matrix that can contains an original 
